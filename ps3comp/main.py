f4f9329836e8c500aa6b7a7788801152cd2601a3
Kappaccinoh <lim.jia28@gmail.com>
2023-09-20 11:04:06 +0800

    cleaning up files

--- ps3comp/main.py
+++ ps3comp/main.py
@@ -1,20 +1,16 @@
-# from negamax_alpha_beta import deepBlue
-# import coursemologysubmission
-# from coursemologysubmission import PlayerAI
-import idscoursemology
-from idscoursemology import PlayerAI
+# Upgraded deepBlue Version 
+import deepbluecoursemology
+from deepbluecoursemology import PlayerAI
+
+# # Primitive Version - just IDS alone
+# import idscoursemology
+# from idscoursemology import PlayerAI
+
+
 from playerNaive import PlayerNaive
 import utils
 
 if __name__ == "__main__":
-    # print(deepBlue([['_', '_', '_', '_', '_', '_'], ['_', '_', 'B', 'B', '_', '_'], ['_', '_', '_', '_', 'B', 'B'], ['W', 'B', 'W', '_', 'B', '_'], ['_', '_', '_', '_', 'W', 'W'], ['_', 'W', 'W', '_', '_', '_']]))
-    # print(deepBlue([['_', '_', '_', '_', 'B', '_'], ['_', '_', '_', 'B', '_', '_'], ['_', '_', 'B', '_', '_', '_'], ['_', 'W', 'W', 'W', '_', '_'], ['_', '_', '_', '_', 'W', '_'], ['_', '_', '_', '_', '_', '_']]))
-    # print(deepBlue([['_', '_', '_', '_', 'B', '_'], ['_', '_', 'B', 'B', '_', '_'], ['_', '_', '_', '_', '_', '_'], ['_', 'W', 'W', 'W', '_', '_'], ['_', '_', '_', '_', 'W', '_'], ['_', '_', '_', '_', '_', '_']]))
-    # board = [['_', '_', '_', '_', '_', '_'], ['_', '_', 'B', 'B', '_', '_'], ['_', '_', '_', '_', 'B', 'B'], ['W', 'B', 'W', '_', 'B', '_'], ['_', '_', '_', '_', 'W', 'W'], ['_', 'W', 'W', '_', '_', '_']]
-    # board1 = [['_', '_', '_', '_', 'B', '_'], ['_', '_', '_', 'B', '_', '_'], ['_', '_', 'B', '_', '_', '_'], ['_', 'W', 'W', 'W', '_', '_'], ['_', '_', '_', '_', 'W', '_'], ['_', '_', '_', '_', '_', '_']]
-    # board2 = [['_', '_', '_', '_', 'B', '_'], ['_', '_', 'B', 'B', '_', '_'], ['_', '_', '_', '_', '_', '_'], ['_', 'W', 'W', 'W', '_', '_'], ['_', '_', '_', '_', 'W', '_'], ['_', '_', '_', '_', '_', '_']]
-    # currAI = PlayerAI()
-    # print(currAI.make_move(board2))
     board = utils.generate_init_state()
     res = utils.play(PlayerAI(), PlayerNaive(), board)
     print(res)
