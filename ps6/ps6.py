087e86c5a8b7274cfed425184ab1b5705fb9b120
Kappaccinoh <lim.jia28@gmail.com>
2023-10-21 20:40:27 +0800

    ps6.py

--- ps6/ps6.py
+++ ps6/ps6.py
@@ -24,27 +24,48 @@ def polyfit(x, y, loss_fn, n, lr, n_iter):
     -------
         Near-optimal coefficients of the nth-degree polynomial as a tensor of shape (1, n+1) after `n_iter` epochs.
     """
-    
-    
+    X = torch.vander(x, n + 1, increasing=True)
+    w = torch.zeros(n + 1, requires_grad=True)
 
+    for i in range(n_iter):
+        yHat = torch.mv(X, w)
+        loss = loss_fn(y, yHat)
 
+        loss.backward()
+
+        with torch.no_grad():
+            w -= lr * w.grad
+            w.grad.zero_()
+
+    return w
+
+# Task 1.3
 x = torch.linspace(-math.pi, math.pi, 1000)
 
 # Original true values
 y = torch.sin(x)
 plt.plot(x, y, linestyle='solid', label='sin(x)')
 
-# MSE
+# MSE - Graph 1
 mse = lambda y_true, y_pred: torch.mean(torch.square(y_pred - y_true))
-a, b, c, d = polyfit(x, y, mse, 3, 1e-3, 5000)
-y_pred_mse = a + b * x + c * x ** 2 + d * x ** 3
-plt.plot(x, y_pred_mse.detach().numpy(), linestyle='dashed', label=f'mse')
+a, b, c, d = polyfit(x, y, mse, 3, 1e-6, 5000)
+y_pred_mse1 = a + b * x + c * x ** 2 + d * x ** 3
+plt.plot(x, y_pred_mse1.detach().numpy(), linestyle='dashed', label=f'mse-1')
 
-# MAE
-mae = lambda y_true, y_pred: torch.mean(torch.abs(y_pred - y_true))
-a, b, c, d = polyfit(x, y, mae, 3, 1e-3, 5000)
-y_pred_mae = a + b * x + c * x ** 2 + d * x ** 3
-plt.plot(x, y_pred_mae.detach().numpy(), linestyle='dashed', label=f'mae')
+# MSE - Graph 2
+a, b, c, d = polyfit(x, y, mse, 3, 1e6, 5000)
+y_pred_mse2 = a + b * x + c * x ** 2 + d * x ** 3
+plt.plot(x, y_pred_mse2.detach().numpy(), linestyle='dashed', label=f'mse-2')
+
+# MSE - Graph 3
+a, b = polyfit(x, y, mse, 1, 1e-3, 5000)
+y_pred_mse3 = a + b * x
+plt.plot(x, y_pred_mse3.detach().numpy(), linestyle='dashed', label=f'mse-3')
+
+# MSE - Graph 4
+a, b, c, d, e, f, g = polyfit(x, y, mse, 6, 1e-3, 5000)
+y_pred_mse4 = a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6
+plt.plot(x, y_pred_mse4.detach().numpy(), linestyle='dashed', label=f'mse-4')
 
 plt.axis('equal')
 plt.title('Comparison of different fits')
@@ -52,22 +73,4 @@ plt.legend()
 plt.show()
 
 if __name__ == "__main__":
-    x = torch.linspace(-math.pi, math.pi, 10)
-    y = torch.sin(x)
-
-    def mse(y_true, y_pred):
-        assert y_true.shape == y_pred.shape, f"Your ground truth and predicted values need to have the same shape {y_true.shape} vs {y_pred.shape}"
-        return torch.mean(torch.square(y_pred - y_true))
-    def mae(y_true, y_pred):
-        assert y_true.shape == y_pred.shape, f"Your ground truth and predicted values need to have the same shape {y_true.shape} vs {y_pred.shape}"
-        return torch.mean(torch.abs(y_pred - y_true))
-
-    test1 = polyfit(x, x, mse, 1, 1e-1, 100).tolist()
-    test2 = polyfit(x, x**2, mse, 2, 1e-2, 2000).tolist()
-    test3 = polyfit(x, y, mse, 3, 1e-3, 5000).tolist()
-    test4 = polyfit(x, y, mae, 3, 1e-3, 5000).tolist()
-
-    assert allclose(test1, [0.0, 1.0], atol=1e-6)
-    assert allclose(test2, [0.0, 0.0, 1.0], atol=1e-5)
-    assert allclose(test3, [0.0, 0.81909, 0.0, -0.08469], atol=1e-3)
-    assert allclose(test4, [0.0, 0.83506, 0.0, -0.08974], atol=1e-3)
+    print("plotting")
